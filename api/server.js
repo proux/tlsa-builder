a721bf6fb49610c3ab1b6d1da09ae512665dda5b
Nico Lindemann <n.lindemann@globit.com>
2019-05-16 21:27:20 +0200

    style: lint

--- api/server.js
+++ api/server.js
@@ -3,30 +3,26 @@ const tls = require('tls')
 
 module.exports = (req, res) => {
   let body = ''
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
-  res.setHeader('Content-Type', 'application/json')
   if (req.method === 'POST') {
     req.on('data', chunk => { body += chunk.toString() })
     req.on('end', () => {
-        body = JSON.parse(body)
-        tls.connect(parseInt(body.port), body.domain, function() { 
-            const tlsaRecord = new TLSARecord(this.getPeerCertificate().raw)
-            res.end(JSON.stringify({
-                record: tlsaRecord.toString(
-                    parseInt(body.usage),
-                    parseInt(body.selector),
-                    parseInt(body.matchingType),
-                    body.domain,
-                    parseInt(body.port),
-                    body.protocol),
-                status: 'ok'
-            }))
-        })
+      body = JSON.parse(body)
+      tls.connect(parseInt(body.port), body.domain, function () {
+        const tlsaRecord = new TLSARecord(this.getPeerCertificate().raw)
+        res.end(JSON.stringify({
+          record: tlsaRecord.toString(
+            parseInt(body.usage),
+            parseInt(body.selector),
+            parseInt(body.matchingType),
+            body.domain,
+            parseInt(body.port),
+            body.protocol),
+          status: 'ok'
+        }))
+      })
     })
   } else if (req.method === 'GET') {
-    res.writeHead(301, { "Location": "https://proux.github.io/tlsa-builder/" })
+    res.writeHead(301, { 'Location': 'https://proux.github.io/tlsa-builder/' })
     res.end()
   } else { res.end() }
 }
